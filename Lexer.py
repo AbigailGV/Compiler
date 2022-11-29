8b85248210ff19c056afc83af06c1419e7a72e3d
Abigail Guadarrama <37154990+AbigailGV@users.noreply.github.com>
2022-11-28 19:15:04 -0600

    Create Lexer.py

---
+++ Lexer.py
@@ -0,0 +1,76 @@
+import ply.lex as lex
+
+reserved = (
+    'BOOL',
+    'INT',
+    'FLOAT',
+    'IF',
+    'ELIF',
+    'ELSE',
+    'WHILE',
+    'AND',
+    'OR',
+    'TRUE',
+    'FALSE',
+)
+
+tokens = reserved + (
+    'ID',
+    'INT_CONST',
+    'FLOAT_CONST',
+    'LESS_THAN',
+    'LESS_EQUAL',
+    'GREATER',
+    'GREATER_EQUAL',
+    'EQUAL',
+    'NOT_EQUAL',
+)
+
+literals = [
+    '(',
+    ')',
+    '{',
+    '}',
+    '+',
+    '-',
+    '*',
+    '/',
+    '^',
+    '=',
+    ';',
+]
+
+t_ignore = ' \t'
+
+
+def t_NEWLINE(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+
+t_LESS_THAN = r'<'
+t_GREATER = r'>'
+t_LESS_EQUAL = r'<='
+t_GREATER_EQUAL = r'>='
+t_EQUAL = r'=='
+t_NOT_EQUAL = r'!='
+t_INT_CONST = r'-?\d+([uU]|[lL]|[uU][lL]|[lL][uU])?'
+t_FLOAT_CONST = r'-?((\d+)(\.\d+)(e(\+|-)?(\d+))? | (\d+)e(\+|-)?(\d+))([lL]|[fF])?'
+
+reserved_map = {}
+for r in reserved:
+    reserved_map[r.lower()] = r
+
+
+def t_ID(t):
+    r'[A-Za-z_][\w_]*'
+    t.type = reserved_map.get(t.value, "ID")
+    return t
+
+
+def t_error(t):
+    print("Illegal character %s" % repr(t.value[0]))
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
